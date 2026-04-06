01be7346fd7be60cfc6ee7bb828a34c304904784
Prathap Selvakumar <prathapsk@hotmail.com>
2026-04-06 13:19:50 +0100

    Vercel Production issue

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production';
+const isVercel = process.env.VERCEL === '1';
 
 const nextConfig = {
   output: "export",
-  basePath: isProd ? "/prathapsk" : "",
+  basePath: (isProd && !isVercel) ? "/prathapsk" : "",
   reactStrictMode: true,
   images: {
     unoptimized: true,
